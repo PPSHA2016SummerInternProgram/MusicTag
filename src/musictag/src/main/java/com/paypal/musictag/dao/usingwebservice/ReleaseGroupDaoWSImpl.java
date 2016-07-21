fb46338dd4435929629cc61504e0a6898e60c883
Justin Jia <justinj656@live.cn>
2016-07-21 18:18:47 +0800

    correct the url and params in release group dao

--- src/musictag/src/main/java/com/paypal/musictag/dao/usingwebservice/ReleaseGroupDaoWSImpl.java
+++ src/musictag/src/main/java/com/paypal/musictag/dao/usingwebservice/ReleaseGroupDaoWSImpl.java
@@ -14,9 +14,9 @@ public class ReleaseGroupDaoWSImpl implements ReleaseGroupDao{
 
 	@Override
 	public Map<String, Object> releases(String releaseGroupId) throws IOException {
-		//http://10.24.53.72:5000/ws/2/release-group/01385daa-0a2d-462a-bdbb-fe9a320d0f13?inc=releases&fmt=json
+		// http://10.24.53.72:5000/ws/2/release-group/01385daa-0a2d-462a-bdbb-fe9a320d0f13?inc=releases&fmt=json
 		Map<String, String> params = new HashMap<>();
 		params.put("inc", "releases");
-		return MusicTagServiceAPI.sendRequest("release", params);
+		return MusicTagServiceAPI.sendRequest("release-group/" + releaseGroupId, params);
 	}
 }
